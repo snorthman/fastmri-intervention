883bf71ea66429a210b52af8395e182ff7e324b0
Stan <s_northman@hotmail.com>
2022-07-19 16:11:14 +0200

    Update cli.py

--- intervention/cli.py
+++ intervention/cli.py
@@ -24,3 +24,11 @@ def prep(settings: Path):
               prompt='Enter path/to/settings.json', default='.', help="Path to json settings file")
 def inference(settings: Path):
     intervention.segment.inference(Settings('inference', settings))
+
+
+@cli.command(name='plot')
+@click.option('-s', '--settings', type=click.Path(resolve_path=True, path_type=Path),
+              prompt='Enter path/to/settings.json', default='.', help="Path to json settings file")
+def plot(settings: Path):
+    s = Settings('plot', settings)
+    intervention.segment.plot(s.dm.predict)
